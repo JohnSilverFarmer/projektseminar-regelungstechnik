50166b46e662971e255eab65600829d3618924f1
Johannes Silberbauer <johannes_david.silberbauer@stud.tu-darmstadt.de>
2019-09-24 10:25:10 +0200

    Update code for plotting viewpoint dependency

--- scripts/plots/viewpoint_dependency.py
+++ scripts/plots/viewpoint_dependency.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from matplotlib import pyplot as plt
+import numpy as np
 
 from scripts.plots.utils import read_results, compute_aggregated_stats
 
@@ -34,12 +35,23 @@ def _aggregate_results(results, with_backup):
 def main():
     results = read_results(Path(RESULT_FILE))
 
-    ags = _aggregate_results(results, with_backup=False)
+    ags = _aggregate_results(results, with_backup=True)
     stats = compute_aggregated_stats(ags)
 
-    plt.bar(stats.parameter, stats.metric_mean, yerr=stats.metric_std)
+    # layout ids are missing number 2 so make 3->2, 4->3 and so on
+    stats.parameter[stats.parameter > 1] -= 1
+    stats.parameter[:] = stats.parameter.astype(np.int)
+
+    fig, ax = plt.subplots(figsize=(4.135, 3))
+    fig.set_size_inches(4.135, 3)
+
+    ax.bar(stats.parameter, stats.metric_mean, yerr=stats.metric_std, error_kw=dict(capsize=6))
+
+    ax.set_xlabel('Perspektive')
+    ax.set_ylabel('Genauigkeit')
+
     plt.tight_layout()
-    plt.show()
+    plt.savefig(Path('~/Desktop/text-erkennung-perspektive.png').expanduser(), bbox_inches='tight', dpi=400)
 
 
 if __name__ == '__main__':
